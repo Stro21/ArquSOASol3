09070c5888b5000847e3eb03169fb753e50dbf48
Eduardo Straub <eduardo.straub1@gmail.com>
2015-11-23 01:21:18 -0300

    se agregaron unas cosas

---
+++ ArquSOASol3/IVenta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ArquSOASol3
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IVenta" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IVenta
+    {
+        [OperationContract]
+        bool Buscar_Producto(string producto);
+    }
+}
---
+++ ArquSOASol3/Venta.svc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ArquSOASol3
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Venta" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Venta.svc o Venta.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class Venta : IVenta
+    {
+        public bool Buscar_Producto(string producto)
+        {
+            bool encontrado = false;
+            return encontrado;
+        }
+    }
+}
